a2ab4785642c6c55d8b0efc0c291428185c0f2ce
mrogman <mrogman@gmail.com>
2015-12-06 17:36:07 -0500

    make app.rb a client dev file

--- app.rb
+++ app.rb
@@ -1,11 +1,17 @@
 require 'sinatra'
 require 'slim'
 require 'sass'
+require 'json'
 
+set :port, 8090
+set :environment, :development
+
+## for development - include compiled html in client
 get '/' do
   slim :index
 end
 
+## for development - include compiled css in client
 get '/styles.css' do
   sass :styles, :style => :expanded
 end
