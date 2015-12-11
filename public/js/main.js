990db5b8c2e6cdeb1acb8d8172ba4ade15fb0aaa
mrogman <mrogman@gmail.com>
2015-12-11 13:57:24 -0500

    create hidden backbone view for category viewer. Input focused on ready and blur triggers category viewer show transition

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,30 @@
+var $searchInput;
+var $categoryViewer;
+
+var categoryView = Backbone.View.extend({
+  initialize: function() {
+    this.render();
+    $categoryViewer = $('.category-viewer');
+    $categoryViewer.hide();
+  },
+  render: function() {
+    var categoryViewTemplate = $('#categoryViewTemplate').html();
+    var template = Handlebars.compile(categoryViewTemplate);
+    var context = {};
+    var html = template(context);
+    this.$el.html(html);
+  },
+});
+
 $(document).ready(function() {
+
+  $searchInput = $("input.search");
+  $searchInput.focus(); //do first!
+
+  var category_viewer_template = new categoryView({el: $('.category-viewer-placeholder') });
+
+  $("input.search").on('blur', function() {
+    $categoryViewer.fadeIn('fast');
+  });
+
 });
