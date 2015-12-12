2271fe77cd5cb3b6151e86b04d32f87b88de7711
mrogman <mrogman@gmail.com>
2015-12-12 00:54:29 -0500

    search bar keydown opens QR window. Somewhat functional mode switching

--- public/js/main.js
+++ public/js/main.js
@@ -3,20 +3,25 @@ var Gateway = {
   init: function() {
     Gateway.renderViews();
     Gateway.getElements();
-    Gateway.$categoryPlaceholder.css({ height: '0' });
+    Gateway.$middle.css({ height: '0' });
 
     Gateway.search.$input.on({
         'focus': function() {
           Gateway.search.$outer.addClass('search-focused', 200);
+          if(!Gateway.quick_results_view.$quickResultsDiv.is(':visible') && Gateway.search.activated) {
+            Gateway.quick_results_view.showQuickResults();
+          }
         },
         'blur': function() {
           Gateway.showCategories();
           Gateway.search.$outer.removeClass('search-focused', 200);
         },
         'keypress': function(e) {
-          if(e.which == 13) {
+          if(e.which == 13) { //enter
             Gateway.search.send();
           }
+          //open quick results div if not activated
+          if(!Gateway.search.activated) Gateway.openQuickResults();
         }
     })
     .focus();
@@ -25,25 +30,35 @@ var Gateway = {
 
   getElements: function() {
     Gateway.$container = $('div.gateway')
-    Gateway.$categoryPlaceholder = $('.category-viewer-placeholder');
+    Gateway.$middle = $('.middle');
 
     Gateway.search.getElements();
   },
 
   renderViews: function() {
-    Gateway.category_view = new categoryView({el: $('.category-viewer-placeholder') });
-    Gateway.shortcut_bar_view = new shortcutBarView({el: $('.shortcut-bar-wrapper') });
+    Gateway.category_view = new categoryView({ el: $('.middle') });
+    Gateway.shortcut_bar_view = new shortcutBarView({ el: $('.shortcut-bar-wrapper') });
+    Gateway.quick_results_view = new quickResultsView({ el: $('.middle') });
   },
 
   showCategories: function() {
-    Gateway.$categoryPlaceholder.animate({
+    if(Gateway.quick_results_view.$quickResultsDiv.is(':visible')) Gateway.quick_results_view.hideQuickResults();
+    Gateway.$middle.animate({
       height: '50vh'
     }, 300, function() {
       Gateway.category_view.$categoryViewer.fadeIn('fast');
     });
   },
 
+  openQuickResults: function() {
+    Gateway.search.activated = true;
+    if(Gateway.category_view.$categoryViewer.is(':visible')) Gateway.category_view.hideCategoryViewer();
+    Gateway.quick_results_view.showQuickResults();
+  },
+
   search: {
+
+    activated: false, //quick result div open
 
     getElements: function(){
       this.$outer = $('div.search');
@@ -61,13 +76,11 @@ var Gateway = {
 
 }
 
-var $shortcutBar;
-
 var shortcutBarView = Backbone.View.extend({
   initialize: function() {
     this.render();
-    $shortcutBar = $('.shortcut-bar');
-    $shortcutBar
+    this.$shortcutBar = $('.shortcut-bar');
+    this.$shortcutBar
       .hide()
       .fadeIn(600);
   },
@@ -76,7 +89,7 @@ var shortcutBarView = Backbone.View.extend({
     var template = Handlebars.compile(shortcutBarTemplate);
     var context = {};
     var html = template(context);
-    this.$el.html(html);
+    this.$el.append(html);
   }
 });
 
@@ -87,13 +100,47 @@ var categoryView = Backbone.View.extend({
     this.$categoryViewer.hide();
   },
   render: function() {
+    console.log('rendering category viewer');
     var categoryViewTemplate = $('#categoryViewTemplate').html();
+    console.log('categoryViewTemplate: ' + categoryViewTemplate);
     var template = Handlebars.compile(categoryViewTemplate);
     var context = {};
     var html = template(context);
-    this.$el.html(html);
+    this.$el.append(html);
   },
+
+  hideCategoryViewer: function() {
+    this.$categoryViewer.fadeOut('fast');
+  }
 });
+
+var quickResultsView = Backbone.View.extend({
+  initialize: function() {
+    this.render();
+    this.$quickResultsDiv = $('.quick-results');
+    this.$quickResultsDiv.hide();
+  },
+
+  render: function() {
+    var quickResultsTemplate = $('#quickResultsTemplate').html();
+    console.log('quickResultsTemplate: ' + quickResultsTemplate);
+    var template = Handlebars.compile(quickResultsTemplate);
+    var context = {};
+    var html = template(context);
+    this.$el.append(html);
+  },
+
+  showQuickResults: function() {
+    if(Gateway.category_view.$categoryViewer.is(':visible')) Gateway.category_view.hideCategoryViewer();
+    this.$quickResultsDiv.fadeIn('fast');
+  },
+
+  hideQuickResults: function() {
+    this.$quickResultsDiv.fadeOut('fast');
+  }
+});
+
+
 
 $(document).ready(function() {
 
