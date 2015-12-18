0adf0ad94f76601f1a8ace19d00f00443e89be21
mrogman <mrogman@gmail.com>
2015-12-18 17:02:26 -0500

    shortcut template typo

--- public/js/shortcutBar.tpl.js
+++ public/js/shortcutBar.tpl.js
@@ -19,7 +19,7 @@ templates['shortcutBar'] = template({"1":function(container,depth0,helpers,parti
 },"4":function(container,depth0,helpers,partials,data) {
     var stack1;
 
-  return ((stack1 = helpers.blockHelperMissing.call(depth0,container.lambda((depth0 != null ? depth0.isFontIcon : depth0), depth0),{"name":"this.isFontIcon","hash":{},"fn":container.program(5, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
+  return ((stack1 = helpers["if"].call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.isFontIcon : depth0),{"name":"if","hash":{},"fn":container.program(5, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
 },"5":function(container,depth0,helpers,partials,data) {
     var stack1, alias1=container.lambda, alias2=container.escapeExpression;
 
