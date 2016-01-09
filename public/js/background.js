21c10934a92db7f0ccfeaa607dfb055d337cd9df
d30xy <mrogman@gmail.com>
2016-01-09 17:33:45 -0500

    background initialization animation

--- public/js/background.js
+++ public/js/background.js
@@ -4,6 +4,23 @@ Background = {
 
   init: function() {
     this.$el = $('body').children('div.background')
+
+    //init animation
+    this.$el
+      .css({
+        opacity: '0',
+        width: '120vw',
+        height: '120vh',
+        top: '-10vh',
+        left: '-10vw'
+      })
+      .animate({
+        opacity: '1',
+        width: '105vw',
+        height: '105vh',
+        top: '-2.5vh',
+        left: '-2.5vw'
+      }, 2000, 'easeOutQuint');
   },
 
   triggerBlur: function() {
